9700b3995a7353f858fa56b0512a390945ba2322
JuliaCanavezzi <jucanavezzi03@gmail.com>
2024-04-19 14:54:48 -0300

    Added Navbar, Footer, Courses, Home and Routers

--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Student } from '../student';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { StudentService } from '../student.service';
 
 @Component({
@@ -8,63 +8,80 @@ import { StudentService } from '../student.service';
   templateUrl: './students.component.html',
   styleUrl: './students.component.css'
 })
-export class StudentsComponent implements OnInit{
- 
-    students: Student [] = [];
-    studentFormGroup: FormGroup ;
-    isEdting : boolean = false;
-  
-    constructor(private formBuilder : FormBuilder,
-                private service: StudentService
-      ){
-      this.studentFormGroup = formBuilder.group({
-        id:[''],
-        name:[''],
-        course:['']
-      });
-    }
-    ngOnInit(): void {
-     this.loadStudents();
-    }
+export class StudentsComponent implements OnInit {
 
-    loadStudents(){
-      this.service.getStudents().subscribe({
-        next: data => this.students = data
-      });
-    }
-  
-    save(){
-      if(this.isEdting){
+  students: Student[] = [];
+  studentFormGroup: FormGroup;
+  isEdting: boolean = false;
+  submitted: boolean = false;
+
+  constructor(private formBuilder: FormBuilder,
+    private service: StudentService
+  ) {
+    this.studentFormGroup = formBuilder.group({
+      id: [''],
+      name: ['', [Validators.minLength(3), Validators.required]],
+      course: ['', Validators.required]
+    });
+  }
+  ngOnInit(): void {
+    this.loadStudents();
+  }
+
+  loadStudents() {
+    this.service.getStudents().subscribe({
+      next: data => this.students = data
+    });
+  }
+
+  save() {
+
+    this.submitted = true;
+
+    if (this.studentFormGroup.valid) {
+      if (this.isEdting) {
         this.service.update(this.studentFormGroup.value).subscribe({
           next: () => {
             this.loadStudents()
             this.isEdting = false;
             this.studentFormGroup.reset();
+            this.submitted = false;
           }
         })
       }
-      else{
-      this.service.save(this.studentFormGroup.value).subscribe  
-      ({
-        next: data => {
-          this.students.push(data)
-          this.studentFormGroup.reset();
-        }
-      });
+      else {
+        this.service.save(this.studentFormGroup.value).subscribe
+          ({
+            next: data => {
+              this.students.push(data)
+              this.studentFormGroup.reset();
+              this.submitted = false;
+            }
+          });
+      }
     }
-    }
+  }
 
-    delete(student: Student){
-      this.service.delete(student).subscribe
+  delete(student: Student) {
+    this.service.delete(student).subscribe
       ({
         next: () => this.loadStudents()
       });
-    }
-
-    update(student: Student){
-      this.isEdting = true;
-      this.studentFormGroup.setValue(student);
-    }
   }
+
+  update(student: Student) {
+    this.isEdting = true;
+    this.studentFormGroup.setValue(student);
+  }
+
+  get name(): any{
+    return this.studentFormGroup.get('name')
+  }
+
+  get course(): any{
+    return this.studentFormGroup.get('course')
+  }
+
+}
 
 
